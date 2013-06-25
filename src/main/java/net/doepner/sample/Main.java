2ecd615440dc7051d2f32d308483193061ec9ae5
admin <odoepner@gmail.com>
2013-06-25 14:38:51 -0300

    Fix first bugs

--- src/main/java/net/doepner/sample/Main.java
+++ src/main/java/net/doepner/sample/Main.java
@@ -1,11 +1,13 @@
 package net.doepner.sample;
 
+import javax.swing.JOptionPane;
+
 /**
  * The class that has the main method
  */
 public class Main {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog("It works!");
+        JOptionPane.showMessageDialog(null, "It works!");
     }
 }
