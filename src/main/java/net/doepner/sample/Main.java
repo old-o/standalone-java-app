cdcdc142a5c0f0e66645de86fdcf5b2f7d60904b
admin <odoepner@gmail.com>
2013-06-25 14:08:09 -0300

    Minimal Maven project for stand-alone Java app

---
+++ src/main/java/net/doepner/sample/Main.java
@@ -0,0 +1,11 @@
+package net.doepner.sample;
+
+/**
+ * The class that has the main method
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("It works!");
+    }
+}
